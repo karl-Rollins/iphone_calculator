a55fe8ceeda3e941ce64bec39c00a932055d9dbd
karl-Rollins <karlrollins@gmail.com>
2026-09-12 18:38:38 +0100

    fix: linter fix

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-unused-vars */
+
 // Function to show the clicked number on the screen
-function appendNumber(number) {
-  const screen = document.getElementById("display-screen");
-  screen.innerText += number;
+function appendNumber (number) {
+  const screen = document.getElementById('display-screen')
+  screen.innerText += number
 }
 
 // Function to clear the screen
-function clearScreen() {
-  document.getElementById("display-screen").innerText = "";
+function clearScreen () {
+  document.getElementById('display-screen').innerText = ''
 }
