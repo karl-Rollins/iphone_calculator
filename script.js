1b54ac7c5d8600f6ff559da85254e0b01e3fe636
karl-Rollins <karlrollins@gmail.com>
2026-09-12 18:26:52 +0100

    fix

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Function to show the clicked number on the screen
 function appendNumber(number) {
-    const screen = document.getElementById('display-screen');
-    screen.innerText += number;
+  const screen = document.getElementById("display-screen");
+  screen.innerText += number;
 }
 
 // Function to clear the screen
 function clearScreen() {
-    document.getElementById('display-screen').innerText = '';
+  document.getElementById("display-screen").innerText = "";
 }
